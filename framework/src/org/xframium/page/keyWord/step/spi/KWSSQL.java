aa0c5d9039076d08e1c122478d390e77b9276ff9
Jim Harrington <jim.r.harrington@g,ail.com>
2016-08-04 23:17:26 -0400

    checkpoint

--- framework/src/org/xframium/page/keyWord/step/spi/KWSSQL.java
+++ framework/src/org/xframium/page/keyWord/step/spi/KWSSQL.java
@@ -22,12 +22,15 @@ package org.xframium.page.keyWord.step.spi;
 
 import java.util.Map;
 import java.util.Iterator;
+import java.util.ArrayList;
 
 import org.openqa.selenium.WebDriver;
 import org.xframium.page.Page;
 import org.xframium.page.data.PageData;
 import org.xframium.page.keyWord.step.AbstractKeyWordStep;
 import org.xframium.page.keyWord.KeyWordDriver;
+import org.xframium.page.keyWord.KeyWordParameter;
+import org.xframium.page.keyWord.KeyWordToken;
 import org.xframium.utility.SQLUtil;
 
 // TODO: Auto-generated Javadoc
@@ -37,6 +40,7 @@ import org.xframium.utility.SQLUtil;
 public class KWSSQL extends AbstractKeyWordStep
 {
     private static final String[] JDBC = new String[] { "jdbc.username", "jdbc.password", "jdbc.url", "jdbc.driverClassName" };
+    private static final String[] EMPTY = new String[0];
 
     /* (non-Javadoc)
      * @see com.perfectoMobile.page.keyWord.step.AbstractKeyWordStep#_executeStep(com.perfectoMobile.page.Page, org.openqa.selenium.WebDriver, java.util.Map, java.util.Map)
@@ -50,12 +54,15 @@ public class KWSSQL extends AbstractKeyWordStep
             throw new IllegalStateException( "Page Object was not defined" );
 
         //
-        // OK, this step needs at least one parameter ans the first is the query to execute.  If there are
+        // OK, this step needs at least one parameter and the first is the query to execute.  If there are
         // any more, they are values to compare the query results to.  If all the test parameters match
         // the step passes
         //
 
         String query = null;
+        String[] query_params = null;
+        KeyWordParameter param1 = null;
+        
         int paramCount = getParameterList().size();
         
         if ( paramCount < 1 )
@@ -64,63 +71,84 @@ public class KWSSQL extends AbstractKeyWordStep
         }
         else
         {
-            Object obj = getParameterValue( getParameterList().get( 0 ), contextMap, dataMap );
-            if ( obj instanceof String )
-            {
-                query = (String) obj;
-            }
-            else
-            {
-                throw new IllegalStateException( "SQL requires a parameter of type String" );
-            }
+            param1 = (( getParameterList().size() == 0 ) ? null : getParameterList().get( 0 ) );
+            query = (( param1 != null ) ? param1.getValue() : null );
+            query_params = loadQueryParams( param1 );
         }
 
         try
         {
-            Map[] resultsArr = SQLUtil.getRow( KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[0] ),
-                                               KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[1] ),
-                                               KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[2] ),
-                                               KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[3] ),
-                                               query,
-                                               null );
-            Map results = resultsArr[0];
-
             //
-            // The Map returned is keyed by the selected column alias AND
-            // the integer column alias.  So, we'll ignore keys of type
-            // Integer when setting contest data
+            // OK, now we need to look at the statement to see if we're reading or writing
             //
             
-            if ( getContext() != null && !getContext().isEmpty() )
+            if ( query.trim().toUpperCase().startsWith( "SELECT" ))
             {
-                Iterator keys = results.keySet().iterator();
-            
-                while( keys.hasNext() )
-                {
-                    Object key = keys.next();
-                    Object value = results.get( key );
-    
-                    if ( key instanceof Integer )
-                    {
-                        continue;
-                    }
+                Map[] resultsArr = SQLUtil.getRow( KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[0] ),
+                                                   KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[1] ),
+                                                   KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[2] ),
+                                                   KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[3] ),
+                                                   query,
+                                                   query_params );
+                Map results = resultsArr[0];
 
-                    String context_name = getContext() + "_" + key;
+                //
+                // The Map returned is keyed by the selected column alias AND
+                // the integer column alias.  So, we'll ignore keys of type
+                // Integer when setting contest data
+                //
+            
+                if ( getContext() != null && !getContext().isEmpty() )
+                {
+                    Iterator keys = results.keySet().iterator();
+            
+                    while( keys.hasNext() )
+                    {
+                        Object key = keys.next();
+                        Object value = results.get( key );
+    
+                        if ( key instanceof Integer )
+                        {
+                            continue;
+                        }
+
+                        String context_name = getContext() + "_" + key;
+                    
+                        if ( log.isDebugEnabled() )
+                            log.debug( "Setting Context Data to [" + value + "] for [" + context_name + "]" );
+                    
+                        contextMap.put( context_name, value );
+                    }
+                }
+
+                for( int i = 1; (( i < paramCount ) && ( rtn )); ++i )
+                {
+                    Object paramValue = getParameterValue( getParameterList().get( i ), contextMap, dataMap );
+                    Object queryValue = results.get( i );
+
+                    rtn = ((( paramValue == null ) && ( queryValue == null )) ||
+                           (( paramValue != null ) && ( paramValue.equals( queryValue ))));
+                }
+            }
+            else
+            {
+                int row_count = SQLUtil.execute( KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[0] ),
+                                                 KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[1] ),
+                                                 KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[2] ),
+                                                 KeyWordDriver.instance().getConfigProperties().getProperty( JDBC[3] ),
+                                                 query,
+                                                 query_params );
+
+                if ( getContext() != null && !getContext().isEmpty() )
+                {
+                    String context_name = getContext() + "_row_count";
+                    String value = "" + row_count;
                     
                     if ( log.isDebugEnabled() )
                         log.debug( "Setting Context Data to [" + value + "] for [" + context_name + "]" );
                     
                     contextMap.put( context_name, value );
                 }
-            }
-
-            for( int i = 1; (( i < paramCount ) && ( rtn )); ++i )
-            {
-                Object paramValue = getParameterValue( getParameterList().get( i ), contextMap, dataMap );
-                Object queryValue = results.get( i );
-
-                rtn = ((( paramValue == null ) && ( queryValue == null )) ||
-                       (( paramValue != null ) && ( paramValue.equals( queryValue ))));
             }
         }
         catch( Throwable e )
@@ -137,6 +165,25 @@ public class KWSSQL extends AbstractKeyWordStep
     public boolean isRecordable()
     {
         return false;
+    }
+
+    //
+    // Helpers
+    //
+
+    private String[] loadQueryParams( KeyWordParameter param )
+    {
+        ArrayList<String> rtn = new ArrayList<String>();
+
+        Iterator<KeyWordToken> tokens = param.getTokenList().iterator();
+        while( tokens.hasNext() )
+        {
+            KeyWordToken token = tokens.next();
+
+            rtn.add( token.getValue() );
+        }
+
+        return rtn.toArray( EMPTY );
     }
 
 }
